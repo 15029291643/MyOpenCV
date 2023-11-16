91f5f1efcd2d26b754eb579c5835000aa9f1e4fd
haojinhui <3526250846@qq.com>
2023-11-16 19:06:41 +0800

    跳转到相机

--- app/src/main/java/com/dome/myopencv/MainActivity.java
+++ app/src/main/java/com/dome/myopencv/MainActivity.java
@@ -2,8 +2,11 @@ package com.dome.myopencv;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
+
+import com.dome.myopencv.databinding.ActivityMainBinding;
 
 import org.opencv.android.OpenCVLoader;
 
@@ -12,9 +15,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         Toast.makeText(this,
                 OpenCVLoader.initDebug() ? "OpenCV加载成功" : "OpenCV加载失败"
                 , Toast.LENGTH_SHORT).show();
+
+
+        binding.button2.setOnClickListener(v -> {
+            Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+            startActivity(intent);
+        });
+
     }
 }
