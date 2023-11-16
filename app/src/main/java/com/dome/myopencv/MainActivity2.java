c72ec26fa3d3be9c444df04dd1b260bb174823be
haojinhui <3526250846@qq.com>
2023-11-16 20:33:16 +0800

    添加新的view

--- app/src/main/java/com/dome/myopencv/MainActivity2.java
+++ app/src/main/java/com/dome/myopencv/MainActivity2.java
@@ -1,19 +1,17 @@
-
 package com.dome.myopencv;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
 import com.dome.myopencv.databinding.ActivityMain2Binding;
 
-class MainActivity2 extends AppCompatActivity {
+public class MainActivity2 extends AppCompatActivity {
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ActivityMain2Binding binding = ActivityMain2Binding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
+        setContentView(binding.getRoot());;
     }
 }
